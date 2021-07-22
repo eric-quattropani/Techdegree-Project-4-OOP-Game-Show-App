e937a4176fcf5fd3775c42864b09962e01151d08
eric-quattropani <eric.quattropani@gmail.com>
2021-07-22 11:14:27 +1000

    Up to Step 6
    addPhraseToDisplay();

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,28 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+    constructor() {
+        this.missed = 0;
+        //found it easier to write out the pgrases like this (clean up p)
+        this.phrases = [
+            new Phrase('A Dime a Dozen'),
+            new Phrase('A Piece of Cake'),
+            new Phrase('An Arm and a Leg'),
+            new Phrase('Barking Up The Wrong Tree'),
+            new Phrase('Beating Around the Bush')
+        ];
+        this.activePhrase = null;
+    }
+    /**
+    * Selects random phrase from phrases property
+    * @return {Object} Phrase object chosen to be used
+    */
+    getRandomPhrase() {
+        const randomPhrase = this.phrases[Math.floor(Math.random() * this.phrases.length)];
+        return randomPhrase;
+    };
+
+
+}
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,30 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase;
+    }
+
+    /**
+    * Display phrase on game board
+    */
+    addPhraseToDisplay() {
+        const letter = this.phrase.split('');
+        const phraseElement = document.querySelector('#phrase ul');
+        phrase.forEach(character => {
+            let listItems;
+            if (character === ' ') {
+                listItems = `<li class="space"></li>`
+            } else {
+                listItems = `<li class="hide letter ${char}" >${char}</li>`
+            }
+            phraseElement.innerHTML += listItems
+        })
+       
+    }
+
+    //Up to step 6
+
+};
